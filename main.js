9cd2d08e5d6332bfdc2c812ca3dae6720d0cf041
tethye <afraibnat001@gmail.com>
2023-07-21 00:50:49 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Full-stack Developer", "Software Engineer", "Guitarist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
